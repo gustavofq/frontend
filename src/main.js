af2abd3eef5c4d84ca54a2fee39deb0c17b82238
guquintana <quintana.gustavo.f@gmail.com>
2024-11-11 20:57:51 -0300

    add js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
-function main(){
-    alert("hola")
+const url = "https://sea-lion-app-4gxod.ondigitalocean.app/users/login";
+const body = {}
+const headers ={
+    'Content-Type': 'application/json',
+    'User-Agent' :"browser"
+}
+
+function  login(){
+    body.email = document.getElementById("user_input").value
+    body.password = document.getElementById("password_input").value
+    fetch(url,{
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body)
+    })
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data =>{
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('hubo un problema con la operación fetch',error);
+    });
 }
